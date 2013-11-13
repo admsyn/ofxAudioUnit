abcd4fdfe8d7f36aaac3eecc3a4efe51c2cee6fd
Adam Carlucci <carlucci.adam@gmail.com>
2013-11-13 16:45:17 -0500

    add addon_config.mk

---
+++ addon_config.mk
@@ -0,0 +1,14 @@
+meta:
+	ADDON_NAME = ofxAudioUnit
+	ADDON_DESCRIPTION = Simple interface for working with Audio Units
+	ADDON_AUTHOR = Adam Carlucci
+	ADDON_TAGS = "sound" "audio" "audio unit" 
+	ADDON_URL = https://github.com/admsyn/ofxAudioUnit
+
+common:
+	ADDON_FRAMEWORKS = AudioUnit
+	ADDON_FRAMEWORKS += CoreMIDI
+		
+osx:
+	ADDON_FRAMEWORKS += CoreAudioKit
+    
